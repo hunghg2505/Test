f810e9e5aaac1bfd49ff00e33dfb30dcd4ccd7ee
duyen.dang2 <duyen.dang2@sotatek.com>
2022-06-03 19:19:41 +0700

    update style guide

--- src/modules/profile/index.tsx
+++ src/modules/profile/index.tsx
@@ -1,10 +1,14 @@
 import ContainerLayout from 'libraries/layouts/container.layout';
+import Button from 'libraries/UI/Button';
 import React from 'react';
 
 function Profile() {
   return (
     <ContainerLayout title="Profile">
       <h1>Hello Profile</h1>
+      <Button className={'AAA'} type="secondary">
+        Button
+      </Button>
     </ContainerLayout>
   );
 }
