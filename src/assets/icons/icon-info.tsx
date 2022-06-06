203e917768759ed0b0285cdcb420c564cc392f91
duyen.dang2 <duyen.dang2@sotatek.com>
2022-06-06 14:18:35 +0700

    update icon help

---
+++ src/assets/icons/icon-info.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const IconInfo = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
+      <path
+        d="M12 21C14.4853 21 16.7353 19.9926 18.3639 18.3639C19.9926 16.7353 21 14.4853 21 12C21 9.51474 19.9926 7.26474 18.3639 5.63604C16.7353 4.00736 14.4853 3 12 3C9.51474 3 7.26474 4.00736 5.63604 5.63604C4.00736 7.26474 3 9.51474 3 12C3 14.4853 4.00736 16.7353 5.63604 18.3639C7.26474 19.9926 9.51474 21 12 21Z"
+        stroke="#BDBDBD"
+        strokeWidth={2}
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 6.25C12.4142 6.25 12.75 6.58579 12.75 7C12.75 7.41421 12.4142 7.75 12 7.75C11.5858 7.75 11.25 7.41421 11.25 7C11.25 6.58579 11.5858 6.25 12 6.25Z"
+        fill="#BDBDBD"
+        stroke="#BDBDBD"
+        strokeWidth="0.5"
+      />
+      <path
+        d="M12 16V10H11.5H11"
+        stroke="#BDBDBD"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11 16H14"
+        stroke="#BDBDBD"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default IconInfo;
