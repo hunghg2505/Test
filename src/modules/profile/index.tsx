36dac8f5190b3937bd6741571e0fb39b77668bfb
duyen.dang2 <duyen.dang2@sotatek.com>
2022-06-06 09:28:31 +0700

    remove demo

--- src/modules/profile/index.tsx
+++ src/modules/profile/index.tsx
@@ -1,20 +1,10 @@
 import ContainerLayout from 'libraries/layouts/container.layout';
-import Input from 'libraries/UI/Input';
-
-import Select from 'libraries/UI/Select';
 import React from 'react';
 
 function Profile() {
   return (
     <ContainerLayout title="Profile">
       <h1>Hello Profile</h1>
-
-      <Select
-        options={[
-          { value: '2', label: 'Option 1' },
-          { value: '4', label: 'Option 2' }
-        ]}
-      />
     </ContainerLayout>
   );
 }
