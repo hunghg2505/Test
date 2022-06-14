3856bd78b78c01a168d65e02d08115d18ce00433
duyen.dang2 <duyen.dang2@sotatek.com>
2022-06-14 13:37:31 +0700

    update login and signup

--- src/modules/auth/sign-in/utils/sign-un.services.ts
+++ src/modules/auth/sign-in/utils/sign-un.services.ts
@@ -1,13 +1,9 @@
+import { API_PATH } from 'utils/api/constant';
 import { ResponseBase } from 'utils/api/api.types';
 import ApiUtils from 'utils/api/api.utils';
 import { SignInReq } from './sign-in.types';
 
-const apiName = {
-  sign_in: '/sign-in',
-  video_stores: '/video-stores'
-};
-
 export const apiSignIn = async (body: SignInReq) => {
-  const res = await ApiUtils.fetch<any, ResponseBase<any>>(apiName.sign_in, body);
+  const res = await ApiUtils.post<any, ResponseBase<any>>(API_PATH.LOGIN, body);
   return res;
 };
--- src/modules/auth/sign-up/index.tsx
+++ src/modules/auth/sign-up/index.tsx
@@ -5,108 +5,127 @@ import InputForm from 'libraries/form/input/input-form';
 import InputPasswordForm from 'libraries/form/input/input-password-form';
 import ContainerLayout from 'libraries/layouts/container.layout';
 import { RegexUtils } from 'utils/regex-helper';
-import ButtonForm from 'libraries/form/button/button-form';
 
 import styles from './styles.module.scss';
+import Button from 'libraries/UI/Button';
+import { useSignUp } from './service';
 
 export default function SignUpPage() {
   const { t } = useTranslation();
 
+  const reqSignUp = useSignUp();
+
   const onSignUp = (values: any) => {
-    console.log({ values });
+    reqSignUp.run(values);
   };
 
   return (
     <ContainerLayout title={t('sign_up.title')}>
       <div className={styles.signUpPage}>
-        <h1 className={styles.titlePage}>{t('sign_up.title')}</h1>
-        <Form layout="vertical" name="basic" onFinish={onSignUp} autoComplete="off">
-          <div className="mb-16">
-            <InputForm
-              label={t('email_address')}
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: t('messages.errors.require', { field: t('email_address') })
-                },
-                {
-                  pattern: new RegExp(RegexUtils.RegexConstants.REGEX_EMAIL),
-                  message: `${t('messages.errors.email_invalid')}`
-                }
-              ]}
-            />
-          </div>
+        <h1 className={styles.titlePage}>{t('welcome_auth')}</h1>
 
-          <div className="mb-16">
-            <InputForm
-              label={t('name')}
-              name="name"
-              rules={[
-                {
-                  required: true,
-                  message: t('messages.errors.require', { field: t('name') })
-                },
-                {
-                  max: 50,
-                  message: t('messages.errors.max', { max: 50 })
-                }
-              ]}
-            />
-          </div>
+        <div className={styles.formWrap}>
+          <h3 className={styles.titleSignUp}>{t('sign_up.title')}</h3>
+          <Form layout="vertical" name="basic" onFinish={onSignUp}>
+            <div className="mb-16">
+              <InputForm
+                label={t('email_address')}
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: t('messages.errors.require', { field: t('email_address') })
+                  },
+                  {
+                    pattern: new RegExp(RegexUtils.RegexConstants.REGEX_EMAIL),
+                    message: `${t('messages.errors.email_invalid')}`
+                  }
+                ]}
+              />
+            </div>
 
-          <div className="mb-16">
-            <InputPasswordForm
-              label={t('password')}
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: t('messages.errors.require', { field: t('password') })
-                }
-              ]}
-            />
-          </div>
+            <div className="mb-16">
+              <InputForm
+                label={t('name')}
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: t('messages.errors.require', { field: t('name') })
+                  },
+                  {
+                    max: 50,
+                    message: t('messages.errors.max', { max: 50 })
+                  }
+                ]}
+              />
+            </div>
 
-          <div className="mb-40">
-            <InputPasswordForm
-              label={t('confirm_password')}
-              name="confirm_password"
-              dependencies={['password']}
-              rules={[
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    if (!value) {
-                      return Promise.reject(
-                        t('messages.errors.require', { field: t('confirm_password') })
-                      );
-                    }
-                    if (getFieldValue('password') === value) {
+            <div className="mb-16">
+              <InputPasswordForm
+                label={t('password')}
+                name="password"
+                rules={[
+                  {
+                    validator: async (_, value) => {
+                      if (!value) {
+                        return Promise.reject(
+                          t('messages.errors.require', { field: t('password') })
+                        );
+                      }
+                      if (value?.length < 8) {
+                        return Promise.reject(t('messages.errors.min', { min: 8 }));
+                      }
+
+                      const regPassword = new RegExp(RegexUtils.RegexConstants.REGEX_PASSWORD);
+
+                      if (!regPassword.test(value)) {
+                        return Promise.reject(t('messages.errors.password_invalid.aa'));
+                      }
+
                       return Promise.resolve();
                     }
-                    return Promise.reject(t('password_not_match'));
                   }
-                })
-              ]}
-            />
+                ]}
+              />
+            </div>
+
+            <div>
+              <InputPasswordForm
+                label={t('confirm_password')}
+                name="confirm_password"
+                dependencies={['password']}
+                rules={[
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value) {
+                        return Promise.reject(
+                          t('messages.errors.require', { field: t('confirm_password') })
+                        );
+                      }
+                      if (getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(t('password_not_match'));
+                    }
+                  })
+                ]}
+              />
+            </div>
+
+            <Button htmlType="submit" className={styles.buttonSubmit} loading={reqSignUp.loading}>
+              {t('continue')}
+            </Button>
+          </Form>
+
+          <div className={styles.actionMore}>
+            <p>
+              <span>By continuing, you’re agreeing to our </span>
+              <a href="/" target="_blank" rel="noopener noreferrer">
+                Terms of Service
+              </a>
+            </p>
           </div>
-
-          <ButtonForm
-            title={t('continue')}
-            htmlType="submit"
-            buttonType="light"
-            borderRadius={16}
-            className={styles.buttonSubmit}
-          />
-        </Form>
-
-        <div className={styles.actionMore}>
-          <p>
-            <span>By continuing, you’re agreeing to our </span>
-            <a href="/" target="_blank" rel="noopener noreferrer">
-              Terms of Service
-            </a>
-          </p>
         </div>
       </div>
     </ContainerLayout>
