22dbeb497047be82c495350adf60de9791c7db31
duyen.dang2 <duyen.dang2@sotatek.com>
2022-07-01 14:12:54 +0700

    clear cache data-subject

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import { useKeycloak } from '@react-keycloak/web';
 import useInitBase from 'hooks/useInitBase';
 import Loading from 'libraries/components/loading';
 import { Suspense, useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import MasterRoute from 'routing/master-routes.routing';
 import { KeyStorage } from 'utils/local-storage.utils';
+import { clearCache } from 'ahooks';
 
 function App() {
   useInitBase();
   const { keycloak, initialized } = useKeycloak();
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    if (!location.pathname?.includes('data-subject')) {
+      clearCache(['data-management']);
+    }
+  }, [location.pathname]);
 
   useLayoutEffect(() => {
     const authData = {
