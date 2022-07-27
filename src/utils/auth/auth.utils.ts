68929f7373b2e90be4b7f320961aec0a3c5b5e6c
duyen.dang2 <duyen.dang2@sotatek.com>
2022-07-27 12:05:10 +0700

    add refresh token keycloak

--- src/utils/auth/auth.utils.ts
+++ src/utils/auth/auth.utils.ts
@@ -1,28 +1,40 @@
-import { API_PATH } from 'utils/api/constant';
+import axios from 'axios';
 import localStorageUtils, { KeyStorage } from 'utils/local-storage.utils';
-import { instance } from '../api/api.utils';
 
 export const refreshTokenApi = async (): Promise<any> => {
   try {
     const tokenInfo: any = localStorageUtils.getObject(KeyStorage.AUTH, null);
 
-    const res: any = await instance.get(API_PATH.AUTH_KEYSLOAK_REFRESH_TOKEN, {
-      headers: {
-        Authorization: 'Bearer ' + tokenInfo?.refreshToken,
-      },
-    });
+    const params: any = {
+      client_id: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
+      grant_type: 'refresh_token',
+      refresh_token: tokenInfo?.refreshToken,
+    };
+
+    const data = Object.keys(params)
+      .map((key: any) => `${key}=${encodeURIComponent(params[`${key}`])}`)
+      .join('&');
+
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data,
+      url: 'https://abc-auth-dev.abc-dev.network/realms/master/protocol/openid-connect/token',
+    };
+
+    const res: any = await axios(options);
 
     const newTokenInfo: any = {
-      accessToken: res?.content?.accessToken,
-      refreshToken: res?.content?.refreshToken,
-      expireTime: Date.now() + res?.content?.expireTime * 1000,
+      accessToken: res?.data?.access_token,
+      refreshToken: res?.data?.refresh_token,
+      expireTime: Date.now() + res?.data?.expires_in * 1000,
       user: {
         loading: true,
       },
     };
 
     setTokenInfo({ ...tokenInfo, ...newTokenInfo });
-    return res?.content?.accessToken;
+    return res?.data?.access_token;
   } catch (error) {
     // console.log('error refresh token', error);
   }
