63bcc6cfbad25ae221bfa2eef4147d59b8201683
duyen.dang2 <duyen.dang2@sotatek.com>
2022-08-02 17:48:44 +0700

    fix unit test

---
+++ src/test/Test.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Test from '../Test';
+
+test('renders learn react link', () => {
+  render(<Test />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
